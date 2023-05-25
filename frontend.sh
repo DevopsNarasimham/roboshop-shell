a721dd12632f219f232d68683c6d3552cf18344c
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-05-25 21:25:45 +0530

    roboshop nginx script

--- frontend.sh
+++ frontend.sh
@@ -1,17 +1,17 @@
 echo -e "\e[31mInstalling Nginx Server (Robobshop Frontend)\e[0m"
-yum install nginx -y
+yum install nginx -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mRemoving Default View of Nginx\e[0m"
-rm -rf /usr/share/nginx/html/*
+rm -rf /usr/share/nginx/html/* &>>/tmp/roboshop.log
 
 echo -e "\e[33mCopying RoboShop Frontend Design\e[0m"
-curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
+curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip &>>/tmp/roboshop.log
 
-echo -e "\e[34mCreating path for roboshop front end\e[om"
-cd /usr/share/nginx/html
+echo -e "\e[34mCreating path for roboshop front end\e[0m"
+cd /usr/share/nginx/html &>>/tmp/roboshop.log
 
 echo -e "\e[35mUnzipping and Installing the copied roboshop frontend file\e[0m"
-unzip /tmp/frontend.zip
+unzip /tmp/frontend.zip &>>/tmp/roboshop.log
 # Need to copy roboshop configure file
-systemctl enable nginx
-systemctl restart nginx
+systemctl enable nginx &>>/tmp/roboshop.log
+systemctl restart nginx &>>/tmp/roboshop.log
