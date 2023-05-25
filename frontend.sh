30bc9619154136681603a9e3dab508731d3cd37d
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-05-25 20:56:24 +0530

    roboshop nginx script

--- frontend.sh
+++ frontend.sh
@@ -1,7 +1,7 @@
 yum install nginx -y
 rm -rf /usr/share/nginx/html/*
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.zip
-cd /usr/share/nginx/html
+cd usr/share/nginx/html
 unzip /tmp/frontend.zip
 # Need to copy roboshop configure file
 systemctl enable nginx
