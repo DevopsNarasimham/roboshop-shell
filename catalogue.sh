23f5ccc6ee9b21f16198b94316559453a52a676e
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-05-28 18:26:03 +0530

    roboshop nginx script

---
+++ catalogue.sh
@@ -0,0 +1,40 @@
+echo -e "\e[31mConfiguring NodeJS Repos\e[0m"
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+
+echo -e "\e[32mInstalling NodeJS Repos\e[0m"
+yum install nodejs -y
+
+echo -e "\e[33mAdd Application User\e[0m"
+useradd roboshop
+
+echo -e "\e[36mRemove Old FileDirectory and create Application Directory\e[0m"
+rm -rf /app
+mkdir /app
+
+echo -e "\e[34mDownload Application Content\e[0m"
+curl -o /tmp/catalogue.zip https://roboshop-artifacts.s3.amazonaws.com/catalogue.zip
+cd /app
+
+echo -e "\e[35mExtract Application Content\e[0m"
+unzip /tmp/catalogue.zip
+cd /app
+
+echo -e "\e[36mInstall NodeJS Dependencies\e[0m"
+npm install
+
+echo -e "\e[31mUpdate SystemD Service\e[0m"
+cp catalogue.service /etc/systemd/system/catalogue.service
+
+echo -e "\e[32mStart Catalogue Service\e[0m"
+systemctl daemon-reload
+systemctl enable catalogue
+systemctl restart catalogue
+
+echo -e "\e[33mCopy Mongodb Repo File\e[0m"
+cp mongodb.repo /etc/yum.repos.d/mongo.repo
+
+echo -e "\e[34mInstall Mongodb \e[0m"
+yum install mongodb-org-shell -y
+
+echo -e "\e[35mLoad Mongodb Schema \e[0m"
+mongo --host mongodb-dev.devpractice.site </app/schema/catalogue.js
