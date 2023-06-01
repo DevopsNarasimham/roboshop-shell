697a8791c143991c21e5269f794f3432fd9be860
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-06-01 20:04:40 +0530

    user shell script

--- user.sh
+++ user.sh
@@ -1,40 +1,40 @@
 echo -e "\e[31mConfiguring NodeJS Repos\e[0m"
-curl -sL https://rpm.nodesource.com/setup_lts.x | bash
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>/tmp/roboshop.log
 
 echo -e "\e[32m Install NodeJS\e[0m"
-yum install nodejs -y
+yum install nodejs -y &>>/tmp/roboshop.log
 
 echo -e "\e[33m Add Application User\e[0m"
-useradd roboshop
+useradd roboshop &>>/tmp/roboshop.log
 
 echo -e "\e[34m Create Application Directory \e[0m"
-rm -rf /app  &>>/tmp/roboshop.log
-mkdir /app
+rm -rf /app  &>>/tmp/roboshop.log &>>/tmp/roboshop.log
+mkdir /app &>>/tmp/roboshop.log
 
 echo -e "\e[35m Download Application Content\e[0m"
-curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip
-cd /app
+curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[36m Extract Application Content\e[0m"
-unzip /tmp/user.zip
-cd /app
+unzip /tmp/user.zip &>>/tmp/roboshop.log
+cd /app &>>/tmp/roboshop.log
 
 echo -e "\e[31m Install NodeJS Dependencies\e[0m"
-npm install
+npm install &>>/tmp/roboshop.log
 
 echo -e "\e[32m Setup SystemD Service  \e[0m"
-cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service
+cp /home/centos/roboshop-shell/user.service /etc/systemd/system/user.service &>>/tmp/roboshop.log
 
 echo -e "\e[33m Start User Service \e[0m"
-systemctl daemon-reload
-systemctl enable user
-systemctl start user
+systemctl daemon-reload &>>/tmp/roboshop.log
+systemctl enable user &>>/tmp/roboshop.log
+systemctl start user &>>/tmp/roboshop.log
 
 echo -e "\e[34m Copy MongoDB Repo file \e[0m"
-cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo
+cp /home/centos/roboshop-shell/mongo.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[35m Install MongoDB Client \e[0m"
-yum install mongodb-org-shell -y
+yum install mongodb-org-shell -y &>>/tmp/roboshop.log
 
 echo -e "\e[36m Load Schema \e[0m"
-mongo --host mongo-dev.devpractice.site </app/schema/user.js
+mongo --host mongo-dev.devpractice.site </app/schema/user.js &>>/tmp/roboshop.log
