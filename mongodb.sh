46cbfe6ef293b0e5f82a279738f94b14fbf8bc31
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-06-01 19:19:02 +0530

    redis shell script

--- mongodb.sh
+++ mongodb.sh
@@ -1,15 +1,11 @@
 echo -e "\e[31mCopy Mongodb Repo File\e[0m"
-cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo
-
-#&>>/tmp/roboshop.log
+cp /home/centos/roboshop-shell/mongodb.repo /etc/yum.repos.d/mongo.repo &>>/tmp/roboshop.log
 
 echo -e "\e[32mInstalling Mongodb\e[0m"
-yum install mongodb-org -y
-
-#&>>/tmp/roboshop.log
+yum install mongodb-org -y &>>/tmp/roboshop.log
 
 echo -e "\e[32mUpdate Mongodb Listen Address\e[0m"
 sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
 echo -e "\e[33mStarting Mongodb Service\e[0m"
-systemctl enable mongod #&>>/tmp/roboshop.log
-systemctl restrart mongod #&>>/tmp/roboshop.log
+systemctl enable mongod &>>/tmp/roboshop.log
+systemctl restrart mongod &>>/tmp/roboshop.log
