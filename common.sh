a4891f7e52e92b8a0a578b7f12d2d9fe4a533eaa
narasimham Josyabhatla <narasimhamurthymca@hotmail.com>
2023-06-16 18:22:26 +0530

    code dry

--- common.sh
+++ common.sh
@@ -3,13 +3,7 @@ nocolor="\e[0m"
 log_file="/tmp/roboshop.log"
 app_path="/app"
 
-nodejs() {
-  echo -e "${color}Configuring NodeJS Repos${nocolor}"
-  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
-
-  echo -e "${color}Installing NodeJS Repos${nocolor}"
-  yum install nodejs -y &>>${log_file}
-
+app_presetup(){
   echo -e "${color}Add Application User${nocolor}"
   useradd roboshop &>>${log_file}
 
@@ -19,22 +13,37 @@ nodejs() {
 
   echo -e "${color}Download Application Content${nocolor}"
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}.zip &>>${log_file}
-  cd ${app_path} &>>${log_file}
 
   echo -e "${color}Extract Application Content${nocolor}"
-  unzip /tmp/${component}.zip &>>${log_file}
   cd ${app_path} &>>${log_file}
+  unzip /tmp/${component}.zip &>>${log_file}
 
-  echo -e "${color}Install NodeJS Dependencies${nocolor}"
-  npm install &>>${log_file}
+}
 
+systemd_setup(){
   echo -e "${color}Update SystemD Service${nocolor}"
   cp /home/centos/roboshop-shell/${component}.service /etc/systemd/system/${component}.service &>>${log_file}
 
   echo -e "${color}Start ${component} Service${nocolor}"
-  systemctl daemon-reload &>>${log_file}
-  systemctl enable ${component} &>>${log_file}
-  systemctl restart ${component} &>>${log_file}
+    systemctl daemon-reload &>>${log_file}
+    systemctl enable ${component} &>>${log_file}
+    systemctl restart ${component} &>>${log_file}
+}
+
+nodejs() {
+  echo -e "${color}Configuring NodeJS Repos${nocolor}"
+  curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>${log_file}
+
+  echo -e "${color}Installing NodeJS Repos${nocolor}"
+  yum install nodejs -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color}Install NodeJS Dependencies${nocolor}"
+  npm install &>>${log_file}
+
+systemd_setup
+
 }
 
 mongo_shchema_setup() {
@@ -46,4 +55,29 @@ mongo_shchema_setup() {
 
   echo -e "${color} Load Schema ${nocolor}"
   mongo --host mongodb-dev.devpractice.site </app/schema/${component}.js &>>${log_file}
+}
+
+mysql_schema_setup(){
+
+  echo -e "${color}Install MySQL Client ${nocolor}"
+    yum install mysql -y &>>${log_file}
+
+    echo -e "${color}Load Schema ${nocolor}"
+    mysql -h mysql-dev.devpractice.site -uroot -pRoboShop@1 < ${app_path}/schema/${component}.sql &>>${log_file}
+
+}
+maven() {
+  echo -e "${color}Install Maven ${nocolor}"
+  yum install maven -y &>>${log_file}
+
+  app_presetup
+
+  echo -e "${color}Download Maven Dependencies ${nocolor}"
+  mvn clean package &>>${log_file}
+  mv target/${component}-1.0.jar ${component}.jar &>>${log_file}
+
+  mysql_schema_setup
+
+  systemd_setup
+
 }
--- shipping.sh
+++ shipping.sh
@@ -1,34 +1,4 @@
-echo -e "\e[31mInstall Maven \e[0m"
-yum install maven -y &>>/tmp/roboshop.log
+source common.sh
+component=shipping
 
-echo -e "\e[32mAdd Application User \e[0m"
-useradd roboshop &>>/tmp/roboshop.log
-
-echo -e "\e[33mCreate Application Directory \e[0m"
-rm -rf /app   &>>/tmp/roboshop.log
-mkdir /app &>>/tmp/roboshop.log
-
-echo -e "\e[34mDownload Application Content \e[0m"
-curl -L -o /tmp/shipping.zip https://roboshop-artifacts.s3.amazonaws.com/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[35mExtract Application Content \e[0m"
-cd /app &>>/tmp/roboshop.log
-unzip /tmp/shipping.zip &>>/tmp/roboshop.log
-
-echo -e "\e[36mDownload Maven Dependencies \e[0m"
-mvn clean package &>>/tmp/roboshop.log
-mv target/shipping-1.0.jar shipping.jar &>>/tmp/roboshop.log
-
-echo -e "\e[31mSetup SystemD File \e[0m"
-cp /home/centos/roboshop-shell/shipping.service /etc/systemd/system/shipping.service &>>/tmp/roboshop.log
-
-echo -e "\e[32mInstall MySQL Client \e[0m"
-yum install mysql -y &>>/tmp/roboshop.log
-
-echo -e "\e[33mLoad Schema \e[0m"
-mysql -h mysql-dev.devpractice.site -uroot -pRoboShop@1 < /app/schema/shipping.sql &>>/tmp/roboshop.log
-
-echo -e "\e[34mStart Shipping Service \e[0m"
-systemctl daemon-reload &>>/tmp/roboshop.log
-systemctl enable shipping &>>/tmp/roboshop.log
-systemctl restart shipping &>>/tmp/roboshop.log
+maven
